c2237624faff9408c31be29211c414b9ca11a581
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-03-08 15:31:18 -0500

    Update 320Lab05.cpp

--- 320Lab05.cpp
+++ 320Lab05.cpp
@@ -8,19 +8,19 @@ using namespace std;
 int main(){
     srand(time(0));
     stree<integer> intTree;
-    stree<integer>::iterator iter1;
     for(int i = 0; i < 10000; i++){
     int b2 = rand() % 7;
     integer outside(b2);
         if(intTree.find(b2) == intTree.end()){
         intTree.insert(outside);
     }  else {
-        iter1 = intTree.find(b2);
-        outside = *iter1;
+        outside = *intTree.find(b2);
         outside.incCount();
-        *iter1 = outside;
+        *intTree.find(b2) = outside;
         }
     }
-    intTree.displayTree(2);
-  return 0;
+    for(stree<integer>::iterator iter = intTree.begin(); iter != intTree.end(); iter++){
+        cout << *iter << endl;
+    }
+    intTree.displayTree(8);
 }
